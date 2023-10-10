482df9af02044696a81e3efec50024d3a56eda0b
Tykitiky <pedrosilva04102000@gmail.com>
2023-10-10 18:40:31 -0300

    Tienda Funkos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar/NavBar"
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-400'>
+        <NavBar/>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>FunkosPe</h1>
+            <div>
+                <button>Ofertas</button>
+                <button>Animes</button>
+                <button>Marvel</button>
+                <button>Destacados</button>
+            </div>
+            
+        </nav>
+    )
+}
+
+export default NavBar
